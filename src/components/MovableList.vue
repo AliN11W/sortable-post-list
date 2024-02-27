31335ca0390e9ee5232ec0b3e534ab0c508c3596
Ali <alinazari.7321@gmail.com>
2024-02-27 20:45:45 +0100

    Revert "minor refactor"
    This reverts commit 31a0e5ef0c1165b5d9689348c122f4968649e234.

--- src/components/MovableList.vue
+++ src/components/MovableList.vue
@@ -41,7 +41,7 @@ const emit = defineEmits<{
   ];
 }>();
 
-const listOrder = toRef(props, "order");
+const order = toRef(props, "order");
 
 /**
  * Using the useSortableList composable to handle the list sorting.
@@ -50,7 +50,7 @@ const listOrder = toRef(props, "order");
  */
 const { orderedItems, moveUp, moveDown } = useSortableList(
   props.items,
-  listOrder,
+  order,
   onOrderUpdateCallback
 );
 
@@ -67,7 +67,7 @@ function onOrderUpdateCallback(
   emit("update:order", {
     order: newOrder,
     details: {
-      movedItem: props.items[listOrder.value[toIndex]],
+      movedItem: props.items[order.value[toIndex]],
       fromIndex,
       toIndex,
     },
